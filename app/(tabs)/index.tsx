bf18771c9d65403f4b7943f22e68dd00d850e3d6
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-08-17 17:31:50 +0530

    add a loading screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,12 @@
 import {
-  Image,
   StyleSheet,
-  Platform,
   View,
   Text,
-  Button,
   ScrollView,
+  Pressable,
+  ActivityIndicator,
 } from "react-native";
-
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { WordPress } from "../../lib/WordPress";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -20,6 +16,7 @@ export default function HomeScreen() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   const [orders, setOrders] = useState([]);
   const [orderStatus, setOrderStatus] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkCredentials = async () => {
@@ -27,10 +24,36 @@ export default function HomeScreen() {
       const password = await AsyncStorage.getItem("vendorPassword");
       if (email && password) {
         setIsLoggedIn(true);
-        fetchOrders(email, password);
+        await fetchOrders(email, password);
       }
+      setIsLoading(false);
     };
     checkCredentials();
+  }, []);
+
+  useEffect(() => {
+    const loadOrders = async () => {
+      const storedOrders = await AsyncStorage.getItem("orders");
+      if (storedOrders) {
+        setOrders(JSON.parse(storedOrders));
+      }
+    };
+
+    const updateOrders = async () => {
+      const email = await AsyncStorage.getItem("vendorEmail");
+      const password = await AsyncStorage.getItem("vendorPassword");
+      if (email && password) {
+        const newOrders = await fetchOrders(email, password);
+        setOrders(newOrders);
+        await AsyncStorage.setItem("orders", JSON.stringify(newOrders));
+      }
+    };
+
+    loadOrders();
+
+    const intervalId = setInterval(updateOrders, 10000); // Fetch every 10 seconds
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
   }, []);
 
   const fetchOrders = async (email: any, password: any) => {
@@ -42,12 +65,7 @@ export default function HomeScreen() {
     const wp = new WordPress(wpConfig);
     const fetchedOrders = await wp.getOrders();
     console.log(fetchedOrders);
-    setOrders(fetchedOrders);
-    // const initialStatus = {};
-    // fetchedOrders.forEach((order: any) => {
-    //   initialStatus[order.id]  = order.status;
-    // });
-    // setOrderStatus(initialStatus);
+    return fetchedOrders;
   };
 
   const handleStatusChange = (orderId: any, status: any) => {
@@ -61,6 +79,14 @@ export default function HomeScreen() {
   };
 
   const handleSave = async (orderId: any) => {};
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   if (!isLoggedIn) {
     return <LoginForm onLoginSuccess={handleLoginSuccess} />;
@@ -84,15 +110,19 @@ export default function HomeScreen() {
           </Text>
         </View>
         {orders.map((order: any) => (
-          <View key={order.id} style={styles.orderRow}>
-            <Text
-              style={styles.columnCustomerName}
-            >{`${order.billing.first_name} ${order.billing.last_name}`}</Text>
-            <Text
-              style={styles.columnAddress}
-            >{`${order.billing.address_1}, ${order.billing.city}, ${order.billing.state}, ${order.billing.postcode}, ${order.billing.country}`}</Text>
-            <Text style={styles.columnStatus}>{order.status}</Text>
-          </View>
+          <Link href="/order" asChild key={order.id}>
+            <Pressable>
+              <View style={styles.orderRow}>
+                <Text
+                  style={styles.columnCustomerName}
+                >{`${order.billing.first_name} ${order.billing.last_name}`}</Text>
+                <Text
+                  style={styles.columnAddress}
+                >{`${order.billing.address_1}, ${order.billing.city}, ${order.billing.state}, ${order.billing.postcode}, ${order.billing.country}`}</Text>
+                <Text style={styles.columnStatus}>{order.status}</Text>
+              </View>
+            </Pressable>
+          </Link>
         ))}
       </View>
     </ScrollView>
@@ -100,6 +130,11 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   titleContainer: {
     alignItems: "center",
     margin: 30,
