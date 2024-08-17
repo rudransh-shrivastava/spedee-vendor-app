920fd251e4a64ba1506ff102a86e1a4a71a9ae4d
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-08-17 16:43:18 +0530

    add a order component

---
+++ app/(tabs)/order.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  Button,
+  ScrollView,
+} from "react-native";
+
+export default function Page() {
+  return (
+    <>
+      <View>
+        <Text>Order</Text>
+      </View>
+    </>
+  );
+}
